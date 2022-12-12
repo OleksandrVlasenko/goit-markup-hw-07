2e45bff2c2b88de1204ae8829a52f8e6cf227aea
OleksandrVlasenko <sania.rapsik@gmail.com>
2022-12-12 20:10:37 +0200

    Add js for open/close

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,44 @@
+// (() => {
+// 	const refs = {
+// 		openModalBtn: document.querySelector("[data-modal-open]"),
+// 		closeModalBtn: document.querySelector("[data-modal-close]"),
+// 		modal: document.querySelector("[data-modal]"),
+// 	};
+
+// 	refs.openModalBtn.addEventListener("click", toggleModal);
+// 	refs.closeModalBtn.addEventListener("click", toggleModal);
+
+// 	function toggleModal() {
+// 		refs.modal.classList.toggle("backdrop--is-hidden");
+// 	}
+// })();
+
 (() => {
-	const refs = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
+	const modal = document.querySelector(".data-modal");
+	const openModalBtn = document.querySelector(".data-modal-open");
+	const closeModalBtn = document.querySelector(".data-modal-close");
+
+	const toggleMenu = () => {
+		const isMenuOpen =
+			openModalBtn.getAttribute("aria-expanded") === "true" || false;
+		openModalBtn.setAttribute("aria-expanded", !isMenuOpen);
+		modal.classList.toggle("backdrop--is-hidden");
+
+		const scrollLockMethod = !isMenuOpen
+			? "disableBodyScroll"
+			: "enableBodyScroll";
+		bodyScrollLock[scrollLockMethod](document.body);
 	};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	openModalBtn.addEventListener("click", toggleMenu);
+	closeModalBtn.addEventListener("click", toggleMenu);
 
-	function toggleModal() {
-		refs.modal.classList.toggle("backdrop--is-hidden");
-	}
+	// Close the mobile menu on wider screens if the device orientation changes
+	window.addEventListener("change", e => {
+		if (!e.matches) return;
+		modal.classList.remove("backdrop--is-hidden");
+		openModalBtn.setAttribute("aria-expanded", false);
+		bodyScrollLock.enableBodyScroll(document.body);
+	});
 })();
+
